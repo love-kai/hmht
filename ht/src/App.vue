bb2437ab8ca1f0521cf7eb4f37c3c9e25f8bbe62
xiaokai <778635235@qq.com>
2020-04-30 16:39:18 +0800

    登录模块

--- ht/src/App.vue
+++ ht/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-APP
+    <router-view></router-view>
+<el-button type="success">成功按钮</el-button>
   </div>
 </template>
 
---
+++ ht/src/views/login/index.vue
@@ -0,0 +1,50 @@
+<template>
+ <div class="container">
+   <el-card class="my-card">
+     <img src="../../assets/images/logo_index.png" alt="">
+     <!-- 表单 -->
+      <el-form :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:236px;margin-right:10px;"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登 录</el-button>
+        </el-form-item>
+      </el-form>
+   </el-card>
+ </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang = "less">
+.container{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+}
+.my-card{
+  width: 400px;
+  height: 350px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-60%);
+}
+img{
+  display: block;
+  width: 200px;
+  margin: 0 auto;
+}
+</style>
